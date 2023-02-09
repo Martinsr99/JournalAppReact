6361217e72445d1e9f7fb598daee00da85296831
Martinsr99 <silesreche.martin@gmail.com>
2023-02-09 16:57:47 +0100

    Add NavBar, sideBar, noteView and imageGallery

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,13 +1,17 @@
+import { Toolbar } from "@mui/material"
 import { Box } from "@mui/system"
+import { NavBar, SideBar } from "../components"
 
 const drawerWidth = 240
 
 export const JournalLayout = ({children}) => {
   return (
     <Box sx={{display:'flex'}}>
-
+      <NavBar drawerWidth={drawerWidth}></NavBar>
+      <SideBar drawerWidth={drawerWidth}></SideBar>
 
     <Box component='main' sx={{flexGrow:1,p:3}}>
+      <Toolbar></Toolbar>
     {children}
     </Box>
     </Box>
