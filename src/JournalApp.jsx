9088d6e93a38d96270d24959598ea00c8346a3f8
Martinsr99 <silesreche.martin@gmail.com>
2023-02-14 10:09:30 +0100

    Configure authSlice and Redux

--- src/JournalApp.jsx
+++ src/JournalApp.jsx
@@ -1,10 +1,10 @@
-import { AppRouter } from "./router/AppRouter"
-
+import { BrowserRouter } from "react-router-dom";
+import { AppRouter } from "./router/AppRouter";
 
 export const JournalApp = () => {
   return (
     <>
-        <AppRouter/>
+        <AppRouter />
     </>
-  )
-}
+  );
+};
