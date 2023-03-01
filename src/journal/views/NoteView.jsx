d747b49923565287b265d45cc4d08e35e5ba71cd
Martinsr99 <silesreche.martin@gmail.com>
2023-03-01 22:15:03 +0100

    Update note

--- src/journal/views/NoteView.jsx
+++ src/journal/views/NoteView.jsx
@@ -1,15 +1,22 @@
 import { SaveOutlined } from "@mui/icons-material";
 import { Button, Grid, TextField, Typography } from "@mui/material";
-import { useMemo } from "react";
-import { useSelector } from "react-redux";
+import { useEffect, useMemo } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { useForm } from "../../hooks/useForm";
+import { setActiveNote } from "../../store/journal/journalSlice";
+import { startSaveNote } from "../../store/journal/thunks";
 import { ImageGallery } from "../components";
 
 export const NoteView = () => {
 
+  const dispatch = useDispatch()
   const {active:note} = useSelector(state => state.journal)
 
   const {body,title,onInputChange,formState,date} = useForm(note)
+
+  const onSaveNote = () => {
+    dispatch(startSaveNote())
+  } 
   
   const dateString = useMemo(() => {
     const newDate = new Date(date)
@@ -17,6 +24,11 @@ export const NoteView = () => {
 
     return 
   }, [date])
+
+  useEffect(() => {
+    dispatch(setActiveNote(formState))
+  }, [formState])
+  
 
   return (
     <Grid
@@ -33,7 +45,7 @@ export const NoteView = () => {
         </Typography>
       </Grid>
       <Grid item>
-        <Button color="primary" sx={{ padding: 2 }}>
+        <Button onClick={onSaveNote} color="primary" sx={{ padding: 2 }}>
           <SaveOutlined sx={{ fontSize: 30, mr: 1 }} />
           Guardar
         </Button>
--- src/store/journal/thunks.js
+++ src/store/journal/thunks.js
@@ -1,7 +1,7 @@
 import {collection, doc, setDoc} from 'firebase/firestore/lite'
 import { FirebaseDB } from '../../firebase/config';
 import { loadNotes } from '../../helpers/loadNotes';
-import { addNewEmptyNote, savingNewNote, setActiveNote, setNotes } from './journalSlice';
+import { addNewEmptyNote, savingNewNote, setActiveNote, setNotes, setSaving } from './journalSlice';
 
 export const startNewNote = () => {
     return async(dispatch, getState) => {
@@ -35,9 +35,26 @@ export const startLoadingNotes = () => {
         let notes = []
         const {uid} = getState().auth
         if(!uid) throw new Error('El UID del usuario no existe')
-
+        
         notes = await loadNotes(uid)
         dispatch(setNotes(notes))
     }
+    
+}
 
+export const startSaveNote = () => { 
+    
+    return async (dispatch,getState) => {
+
+        dispatch(setSaving())
+
+        const {uid} = getState().auth
+        const {active:note} = getState().journal
+
+        const noteToFireStore = {...note}
+        delete noteToFireStore.id
+
+        const docRef = doc(FirebaseDB,`${uid}/journal/notes/${note.id}`)
+        await setDoc(docRef, noteToFireStore, {merge: true})
+    }
 }
